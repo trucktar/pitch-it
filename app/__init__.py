fbde89e1b6d249cc3353c698d2c4de49a2ae2284
Nyota Mwangi <nyota.benjamin@gmail.com>
2020-02-17 16:54:53 +0300

    Create application factory and initialize extensions

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import config_options
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+
+# The name of the view to redirect to when login is required.
+login_manager.login_view = "auth.login"
+# The message to flash when a user is redirected to the login page.
+login_manager.login_message = ""
+
+
+def create_app(config_name="development"):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    from app.auth import bp as auth_bp
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    from app.main import bp as main_bp
+
+    app.register_blueprint(main_bp)
+
+    return app
